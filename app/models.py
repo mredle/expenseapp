ff597b782ef4c537f17c6d9907366bd9e78a3535
Manuel Redle <coding@nuelredle.ch>
2022-09-24 19:30:08 +0200

    fix error with password recovery

--- app/models.py
+++ app/models.py
@@ -988,7 +988,7 @@ class User(PaginatedAPIMixin, UserMixin, Entity, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
