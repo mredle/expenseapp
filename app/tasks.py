ca2dd02c73d59149c0278e86de2f6f5e8d1a79fd
Manuel Redle <manuel@redle.ch>
2026-03-02 23:47:30 +0100

    fix rq jobs

--- app/tasks.py
+++ app/tasks.py
@@ -27,7 +27,7 @@ def _set_task_progress(progress):
         job.meta['progress'] = progress
         job.save_meta()
         task = Task.query.get(job.id)
-        task.user.add_notification('task_progress', {'task_id': job.get_id(),
+        task.user.add_notification('task_progress', {'task_id': job.id,
                                                      'progress': progress})
         if progress >= 100:
             task.complete = True
