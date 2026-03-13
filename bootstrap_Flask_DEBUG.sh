739f53b15291829092c80cb175c5c4924d494ee0
Manuel Redle <manuel@redle.ch>
2026-03-13 23:46:09 +0100

    update dev scripts

--- bootstrap_Flask_DEBUG.sh
+++ bootstrap_Flask_DEBUG.sh
@@ -3,14 +3,19 @@
 docker-compose -f scripts/dev/docker-compose.yml  up -d
 source create_venv_pyenv.sh
 
-export FLASK_APP=./expenseapp.py
+export FLASK_APP="./expenseapp.py"
 export FLASK_DEBUG=1
-export DB_TYPE=mysql
-export DB_HOST=localhost
+export DB_TYPE="mysql"
+export DB_HOST="localhost"
 export DB_PORT=3306
-export DB_USER=user
-export DB_PW=pw
-export DB_NAME=expenseapp
+export DB_USER="user"
+export DB_PW="pw"
+export DB_NAME="expenseapp"
+export S3_BUCKET_NAME="expenseapp-bucket"
+export S3_REGION="eu-central-1"
+export AWS_ACCESS_KEY_ID="minioadmin"
+export AWS_SECRET_ACCESS_KEY="minioadminpw"
+export S3_ENDPOINT_URL="http://localhost:9000"
 
 while true; do
     sleep 10
